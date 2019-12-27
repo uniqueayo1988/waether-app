58afff7143ec87b66620986a510e359f7a79c6ed
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-12-27 10:33:38 +0100

    Add user address

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ request({
   url: 'http://maps.googleapis.com/maps/api/geocode/json?address=4%20ibidun%20street%20ojuelegba',
   json: true
 }, (error, response, body) => {
-  console.log(error, 'error...')
-  console.log(response, 'response...')
-  console.log(body, 'body...')
+  // console.log(error, 'error...')
+  // console.log(response, 'response...')
+  // console.log(body, 'body...')
   // Pretty Print
-  console.log(JSON.stringify(error, undefined, 2))
+  // console.log(JSON.stringify(error, undefined, 2))
+  console.log(`Address: ${body.results[0].formatted_address}`)
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 })
 
 // err - OVER_QUERY_LIMIT
